569a0242caf20c6b9c8e6c62a530d43acecf4df3
chiranjeevi583 <gcpchiranjeevi@gmail.com>
2024-11-03 13:07:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,42 @@
 provider "google" {
-  project = "my-project-279-436907" // Replace with your GCP project ID
-  region  = "us-central1"
-  zone    = "us-central1-f"
+  project     = "my-project-279-436907"
+  region      = "us-central1"
 }
 
-resource "google_compute_network" "custom_vpc" {
-  name                    = "test-custom-jenkins-vpc"
+resource "google_compute_network" "custom-test" {
+  name                    = "test-network"
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "custom_subnet" {
-  name          = "test-cmd-subnets"
-  ip_cidr_range = "10.0.0.0/24"
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
-  network       = google_compute_network.custom_vpc.id
+  network       = google_compute_network.custom-test.name
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "instance-jenkins-vpc-subnet"
+resource "google_compute_instance" "default" {
+  name         = "instance-vpc-automation"
   machine_type = "e2-medium"
-  zone         = "us-central1-f"
+  zone         = "us-central1-c"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11" // Replace with desired image
+      image = "debian-cloud/debian-11"
     }
   }
 
   network_interface {
-    subnetwork = google_compute_subnetwork.custom_subnet.id
+    network    = google_compute_network.custom-test.name
+    subnetwork = google_compute_subnetwork.network-with-private-secondary-ip-ranges.name
+
     access_config {
-      // This block enables an ephemeral public IP
+      // Ephemeral public IP
     }
   }
 
   service_account {
-    email  = "jenkins@my-project-279-436907.iam.gserviceaccount.com" // Replace with your service account email
+    email  = "jenkins@my-project-279-436907.iam.gserviceaccount.com"
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
